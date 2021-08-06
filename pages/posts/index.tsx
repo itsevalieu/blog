7fde27128ad94b8acf485bbf592b84fce1d47895
Eva Lieu <itsevalieu@gmail.com>
2021-08-05 23:18:07 -0700

    Created landing pages for blog, issues, posts, projects

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.scss";
+import styles from "../../styles/Home.module.scss";
 import Link from "next/link";
 
-export default function Home() {
+export default function Posts() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Eva Lieu</title>
+        <title>Blog | Posts</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Blog for Eva" />
@@ -15,35 +15,18 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello, I'm Eva</h1>
+        <h1 className={styles.title}>Welcome to my blog</h1>
 
         <p className={styles.description}>
           Check out some of my blog posts, projects, or newsletters below.
         </p>
-
-        <div className={styles.grid}>
-          <Link href="/posts">
-            <a className={[styles.card, styles.card_post].join(" ")}>
-              <h2>Posts &rarr;</h2>
-              <p>
-                Thoughts and musings on programming, recent events, or life in
-                general.
-              </p>
-            </a>
-          </Link>
-          <Link href="/projects">
-            <a className={[styles.card, styles.card_project].join(" ")}>
-              <h2>Projects &rarr;</h2>
-              <p>Projects that I've worked on as a full stack developer.</p>
-            </a>
-          </Link>
-          <Link href="/issues">
-            <a className={[styles.card, styles.card_issue].join(" ")}>
-              <h2>Issues &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
+        <div>
+          <Link href="/">
+            <a>Back Home</a>
           </Link>
         </div>
+
+        <div className={styles.grid}></div>
       </main>
 
       <footer className={styles.footer}>
