d8113018f83619a824d2b0d310670026378fa0c3
Eva Lieu <itsevalieu@gmail.com>
2021-08-09 21:43:43 -0700

    Adding links to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/posts">
+        <a>Blog</a>
+      </Link>
+      <Link href="/portfolio">
+        <a>Portfolio</a>
+      </Link>
+      <Link href="/issues">
+        <a>Newsletter</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
+      <Link href="/resume">
+        <a>Resume</a>
+      </Link>
+    </header>
+  );
 }
