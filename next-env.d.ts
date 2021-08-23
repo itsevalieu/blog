364f1df3cb04496006dfa02661de3675eb43c4bd
Eva Lieu <itsevalieu@gmail.com>
2021-08-22 23:19:21 -0700

    Minor styling for h1-6 tags, import components to index file

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
-// / <reference types="next" />
+/// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
 declare module "*.scss";
 declare module "next/*";
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Head from "next/head";
 import styles from "@styles/Home.module.scss";
-import Link from "next/link";
 import List from "@components/List/List";
 import Footer from "@components/Footer/Footer";
+import Header from "@components/Header/Header";
+import Hero from "@components/Hero/Hero";
+import Subscribe from "@components/Subscribe/Subscribe";
+import Teaser from "@components/Teaser/Teaser";
 
 export default function Posts() {
   const [items, setItems] = useState<any[]>([
@@ -25,6 +28,24 @@ export default function Posts() {
       excerpt: "This is an excerpt of cat's cradle.",
       date: "10/10/10",
     },
+    {
+      id: 4,
+      title: "Cat's Cradle4",
+      excerpt: "This is an excerpt of cat's cradle.",
+      date: "10/10/10",
+    },
+    {
+      id: 5,
+      title: "Cat's Cradle5",
+      excerpt: "This is an excerpt of cat's cradle.",
+      date: "10/10/10",
+    },
+    {
+      id: 6,
+      title: "Cat's Cradle6",
+      excerpt: "This is an excerpt of cat's cradle.",
+      date: "10/10/10",
+    },
   ]);
   return (
     <div className={styles.container}>
@@ -35,22 +56,18 @@ export default function Posts() {
         <meta name="description" content="Blog for Eva" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to my blog</h1>
-
-        <p className={styles.description}>
-          Check out some of my blog posts, projects, or newsletters below.
-        </p>
-        <div>
-          <Link href="/">
-            <a>Back Home</a>
-          </Link>
-        </div>
-        <List items={items} />
-        <div className={styles.grid}></div>
+        <Hero>
+          <h1 className={styles.title}>Welcome to my blog</h1>
+          <p className={styles.description}>
+            Check out some of my blog posts, projects, or newsletters below.
+          </p>
+        </Hero>
+        <Subscribe />
+        <Teaser items={items.slice(0, 3)} />
+        <List items={items.slice(3)} />
       </main>
-
       <Footer />
     </div>
   );
