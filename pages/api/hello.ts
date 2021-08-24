f6620f629e33a41bccff21f309d9e289ccbb4510
Eva Lieu <itsevalieu@gmail.com>
2021-08-23 21:59:37 -0700

    Add pseudocode for using github issues api for blog posts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
+
+/**
+ * API endpoint: /posts -> github issues
+ * Params: {
+ * username
+ * pagination(?)
+ * }
+ *
+ * Returns: {
+ * title
+ * description
+ * content
+ * labels
+ * comments
+ * date published
+ * date updated
+ * author
+ * }
+ *
+ */
